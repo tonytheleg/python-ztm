576f4704395341ca381c39c2fb6be7038b08b000
tonytheleg <anatale@protonmail.com>
2020-03-01 18:23:49 -0500

    first iteration of portfolio

---
+++ portfolio/server.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
